3ec589885bd16c59522e79fd1bc7b88c92c4a163
djmolloy57 <dj_molloy57@hotmail.com>
2022-05-19 22:02:06 +0000

    add

--- test_website2.py
+++ test_website2.py
@@ -1,6 +1,6 @@
 import unittest
 from app import app
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 
 class TestOnlineBookStore(unittest.TestCase):
 
@@ -19,6 +19,19 @@ class TestOnlineBookStore(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<h4>SciFi Book List</h4>', response.data)
 
+    def test_thriller(self):
+        app.app_context
+        client = app.test_client(self)
+        response = client.get('/get_thriller', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h4>Thriller Book List</h4>', response.data)
+    
+    def test_biography(self):
+        app.app_context
+        client = app.test_client(self)
+        response = client.get('/get_biography', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h4>Biography Book List</h4>', response.data)
 
     def test_history(self):
         app.app_context
@@ -27,8 +40,14 @@ class TestOnlineBookStore(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<h4>History Book List</h4>', response.data)
 
-    
-    
+    def test_fantasy(self):
+        app.app_context
+        client = app.test_client(self)
+        response = client.get('/get_fantasy', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h4>Fantasy Book List</h4>', response.data)
+
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
